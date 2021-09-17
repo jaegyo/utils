3e7770bcad9e80f0c817f2a1deb7d4094c8b6475
jaegyo <jeangjg@naver.com>
2021-09-17 19:08:56 +0900

    Update extract_info_from_csf_file.py

--- extract_info_from_csf_file.py
+++ extract_info_from_csf_file.py
@@ -12,40 +12,34 @@ root = tree.getroot()
 print(f'-------------')
 print(f'modbus memory')
 print(f'-------------')
-for ModbusMemory in root.iter('ModbusMemory'):
-    for Memory in ModbusMemory.iter('Memory'):
-        for cMemoryInfo in Memory.iter('cMemoryInfo'):
-            SlaveNum = cMemoryInfo.find('SlaveNum').text
-            SlaveUse = cMemoryInfo.find('SlaveUse').text
-            if SlaveUse == 'false':
-                continue
-            SlaveDesc = cMemoryInfo.find('SlaveDesc').text
-            print(f'{SlaveNum}, {SlaveUse}, {SlaveDesc}')
+for cMemoryInfo in root.findall('./ModbusMemory//cMemoryInfo'):
+    SlaveNum = cMemoryInfo.find('SlaveNum').text
+    SlaveUse = cMemoryInfo.find('SlaveUse').text
+    if SlaveUse == 'false':
+        continue
+    SlaveDesc = cMemoryInfo.find('SlaveDesc').text
+    print(f'{SlaveNum}, {SlaveUse}, {SlaveDesc}')
 
 print(f'-------------')
 print(f'network channel')
 print(f'-------------')
-for NetworkChannel in root.iter('NetworkChannel'):
-    for ChannelParameter in NetworkChannel.iter('ChannelParameter'):
-        for cChannelParameter in ChannelParameter.iter('cChannelParameter'):
-            ChannelNum = cChannelParameter.find('ChannelNum').text
-            USE = cChannelParameter.find('USE').text
-            if USE == 'false':
-                continue
-            ChannelType = cChannelParameter.find('ChannelType').text
-            print(f'{ChannelNum}, {USE}, {ChannelType}')
+for cChannelParameter in root.findall('./NetworkChannel//cChannelParameter'):
+    ChannelNum = cChannelParameter.find('ChannelNum').text
+    USE = cChannelParameter.find('USE').text
+    if USE == 'false':
+        continue
+    ChannelType = cChannelParameter.find('ChannelType').text
+    print(f'{ChannelNum}, {USE}, {ChannelType}')
 
 
 print(f'-------------')
 print(f'process list')
 print(f'-------------')
-for ProcessList in root.iter('ProcessList'):
-    for Process in ProcessList.iter('Process'):
-        for cCSProcess in Process.iter('cCSProcess'):
-            ProcessNum = cCSProcess.find('ProcessNum').text
-            ProcessType = cCSProcess.find('ProcessType').text
-            if ProcessType == 'IDLE':
-                continue
-            print(f'{ProcessNum}, {ProcessType}')
+for cCSProcess in root.findall('./ProcessList//cCSProcess'):
+    ProcessNum = cCSProcess.find('ProcessNum').text
+    ProcessType = cCSProcess.find('ProcessType').text
+    if ProcessType == 'IDLE':
+        continue
+    print(f'{ProcessNum}, {ProcessType}')
 
 print('process end')
